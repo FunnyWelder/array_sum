32d1f945fe57c386dae9942000bf42ff1552e2ab
FunnyWelder <56418594+FunnyWelder@users.noreply.github.com>
2019-12-26 19:51:12 +0900

    Little changes

---
+++ src/lib.h
@@ -0,0 +1,6 @@
+int array_sum(unsigned int n, int *arr);
+
+#ifndef LAB_3_LIB_H
+#define LAB_3_LIB_H
+
+#endif //LAB_3_LIB_H
--- tests.cpp
+++ tests.cpp
@@ -2,29 +2,31 @@
 #include "lib.h"
 
 TEST(array_sum, expectedOne){
-unsigned int n;
-int z;
-int *arr;
-n = 1;
-arr[0] = 1;
-z = (array_sum(n, arr));
-EXPECT_EQ(1, 1, z);
+    unsigned int n;
+    int z;
+    int *arr;
+    int sum;
+    n = 1;
+    arr[0] = 1;
+    sum = arr[0];
+    z = (array_sum(n, arr));
+    EXPECT_EQ(sum, z);
 }
 
-
-
 TEST(array_sum, expectedFive){
-unsigned int n;
-int z;
-int *arr;
-n = 5;
-arr[0] = 1;
-arr[1] = 2;
-arr[2] = 3;
-arr[3] = 4;
-arr[4] = 5;
-z = (array_sum(n, arr));
-EXPECT_EQ(5, 1, 2, 3, 4, 5, z);
+    unsigned int n;
+    int z;
+    int *arr;
+    int sum;
+    n = 5;
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    z = (array_sum(n, arr));
+    sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+    EXPECT_EQ(sum, z);
 }
 
 int main(int argc, char **argv) {
